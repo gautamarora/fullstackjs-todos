eebb378745f951445be7d6778f819ce11140ee72
Gautam Arora <gautamsarora3@gmail.com>
2016-06-15 19:07:18 -0400

    List todos from array

--- client/script.js
+++ client/script.js
@@ -1,13 +1,104 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
+
 var TodoApp = React.createClass({
   render: function() {
     return(
-      <div>Hello React!</div>  
+      <div className="well todos">
+        <TodoAdd />
+        <TodoList data={this.props.data} />
+        <div class="row">
+          <TodoCounter />
+          <TodoFilter />
+          <TodoClear />
+        </div>
+      </div>
     );
   }
 });
+
+var TodoAdd = React.createClass({
+  render: function() {
+    return(
+      <form action="#" method="post">
+        <div className="form-group input-group">
+          <input id="add-todo-text" className="form-control" type="text" value="" placeholder="Add a Todo" />
+          <span className="input-group-btn">
+            <input className="btn btn-default" type="button" value="add" />
+          </span>
+        </div>
+      </form>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+      var todos = this.props.data.map(function(todo) {
+        return (
+          <Todo key={todo._id}>{todo.text}</Todo>
+        );
+      });
+      return (
+        <ul className="list-group">
+          {todos}
+        </ul>
+      );
+  }
+});
+
+var Todo = React.createClass({
+  render: function() {
+    return(
+      <li className="list-group-item">
+        <input type="checkbox" defaultChecked="true"/>
+        <span contenteditable="true" class="checked">{this.props.children}</span>
+        <a className="pull-right"><small><i className="glyphicon glyphicon-trash"></i></small></a>
+      </li>
+    );
+  }
+});
+
+var TodoCounter = React.createClass({
+  render: function() {
+    return(
+      <div className="col-xs-12 col-sm-4 text-center">
+        <span className="count">3</span> todos
+      </div>
+    );
+  }
+});
+
+var TodoFilter = React.createClass({
+  render: function() {
+    return(
+      <div className="col-xs-12 col-sm-4 text-center filter">
+          <a className="show-all">all</a> | 
+          <a className="show-not-done"> not done</a> | 
+          <a className="show-done"> done</a>
+      </div>
+    );
+  }
+});
+
+var TodoClear = React.createClass({
+  render: function() {
+    return(
+      <div class="col-xs-12 col-sm-4 text-center">
+        <a class="clear">clear</a>
+      </div>
+    );
+  }
+});
+
+var data = [
+  {_id: 1, text: 'first todo', done: false},
+  {_id: 2, text: 'second todo', done: true},
+  {_id: 3, text: 'third todo', done: false},
+];
+
 ReactDOM.render(
-  <TodoApp />,
+  <TodoApp data={data}/>,
   document.getElementById('app')
 );
