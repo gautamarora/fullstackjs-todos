6bac747716fcd0f3c2557b08d0dce974c46f6e19
Gautam Arora <gautamsarora3@gmail.com>
2016-07-30 16:18:09 -0400

    Switch to bootstrap-sass

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/bootstrap-sass/assets/fonts')));
 
 app.use('/', routes);
 app.use('/users', users);
