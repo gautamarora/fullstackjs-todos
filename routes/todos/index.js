a7a4d056b6bdeac7a42105eff1f5004e07778bbd
Gautam Arora <gautamsarora3@gmail.com>
2016-08-05 16:40:35 -0400

    Update todos to include user (#3)

--- routes/todos/index.js
+++ routes/todos/index.js
@@ -3,10 +3,17 @@ var router = express.Router();
 
 var Todo = require('../../models/todos');
 
-router.get('/', function(req, res, next) {
-  Todo.findAsync({}, null, {sort: {"_id":1}})
+var isLoggedIn = function(req, res, next) {
+  if(!req.user) {
+    return res.redirect('/');
+  }
+  next();
+}
+
+router.get('/', isLoggedIn, function(req, res, next) {
+  Todo.findAsync({user: req.user._id},null,{sort: {"_id":1}})
   .then(function(todos) {
-    res.render('todos', {title: 'Todos', todos: todos});
+    res.render('todos', {title: 'Todos', todos: todos, user: req.user});
   })
   .catch(next);
 });
